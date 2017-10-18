64f79ffa01e9e1caa73c51c7af58d97c78e1017c
aleksander002 <27806518+aleksander002@users.noreply.github.com>
2017-10-18 13:52:37 +0200

    Update login.php

--- engine/login.php
+++ engine/login.php
@@ -1,1 +1,28 @@
-//
+<?php
+require_once("config.php");
+session_start();
+$login=$_POST["login"];
+$password=$_POST["password"];
+$password=sha1(sha1($password));
+$connect = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+if(mysqli_connect_errno()==0)
+{
+  $result=$connect->query("SELECT * From users$dbprefix WHERE login='$login' AND password='$password'");
+  if(mysqli_num_rows($result) > 0){
+    $row=$result->fetch_assoc();
+    $_SESSION['login']=$login;
+    $_SESSION['permissions']=$row['permissions'];
+    $_SESSION['storage']=$row['storage'];
+    $_SESSION['usedspace']=$row['usedspace'];
+    header("Location: filemanager.php");
+		die();
+  }
+  else{
+    session_unset();
+    session_destroy();
+    header("Location: loginform.php?err=1");
+    die();
+  }
+}
+
+?>
