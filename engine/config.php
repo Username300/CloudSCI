0ea28dd934984430c6d7150e1771b0e00fa22062
aleksander002 <27806518+aleksander002@users.noreply.github.com>
2017-10-20 18:56:48 +0200

    Update config.php

--- engine/config.php
+++ engine/config.php
@@ -3,6 +3,8 @@ $dbhost="localhost";
 $dbusername="root";
 $dbpassword="";
 $dbname="cloud";
-$dbprefix="";
-$default_storage=1048576; //rozmiar domyslny przydzielanej pamieci dla uzytkownika (w KB)
+$dbprefix=""; //wystepuje po nazwie tabeli (BEZ SPACJI)
+$default_storage=1048576; //rozmiar domyslny przydzielanej pamieci dla uzytkownika (w KB) - 1GB
+$target_dir = "../files/"; //katalog z plikami
+$max_file_size = 102400; //maksymalny rozmiar pojedynczego pliku (w KB) - 100MB
 ?>
