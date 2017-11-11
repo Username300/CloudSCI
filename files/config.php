52bc28a1905c974fa73574e69f9bdd223835a60c
kamilawk19 <bloghoran@gmail.com>
2017-11-11 16:27:52 +0100

    Add files via upload

---
+++ files/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$project_title = "CloudS";
+
+$dbhost="localhost";
+$dbusername="root";
+$dbpassword="";
+$dbname="cloud";
+$dbprefix='';
+$default_storage=1048576; //rozmiar domyslny przydzielanej pamieci dla uzytkownika (w KB)
+
+
+?>
