907e4c6a6400241c907951e7beb10693f63be8df
kamilawk19 <bloghoran@gmail.com>
2017-11-21 16:36:13 +0100

    Update deletefileform.php

--- engine/deletefileform.php
+++ engine/deletefileform.php
@@ -21,32 +21,43 @@ if(mysqli_connect_errno()==0)
 <!DOCTYPE HTML>
 <html lang="pl">
   <head>
-	   <title>Usuń plik - The Cloud Project</title>
+	<!--
+  //////////////////////////////
+  //                          //
+  // Pamiętaj, by dodać CSS   //
+  //   i pliki bootstrapa     //
+  //                          //
+  //////////////////////////////
+  -->
+	   <title>Usuń plik - <?php echo $project_title;?></title>
 	   <meta charset="utf-8">
+	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	   <link rel="stylesheet" href="..\css\delete_file.css">
+	   
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	
+	<!-- Font-Awesome -->
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+	   
   </head>
   <body>
-      <h1>Usunąć plik
-        <?php
-        echo $filename;
-        ?>
-       ?</h1><br>
-       <h2>Uwaga, usuwanie jest nieodwracalne!</h2><br>
-       <form action="deletefile.php" method="post">
-         <input type="hidden" name="id" value="
-         <?php
-         echo $id;
-         ?>
-         "/>
-         <input type="hidden" name="pid" value="
-         <?php
-         echo $pid;
-         ?>
-         "/>
-         <button type='submit'>Usuń na zawsze</button>
-       </form>
-       <form method="get" action="filemanager.php">
-         <input type="hidden" name="pid" value="<?php echo $pid; ?>">
-       <button type='submit'>Anuluj</button>
-       </form>
+      <div class="container con">
+		
+	
+			<img src="../img/question.jpg" alt="img" class="img left">
+		
+			<div class="left">
+				  <h1>Usunąć plik <?php echo $filename; ?> ?</h1><br>
+				   
+				  <h3>Uważaj! Usuwanie jest nieodwracalne!</h3><br>
+					<form action="deletefile.php" method="post" class="left">
+						 <input type="hidden" name="id" value="<?php echo $id;?>"/>
+						 <input type="hidden" name="pid" value="<?php echo $pid; ?>"/>
+						 <button type='submit' class="btn btn-danger">Usuń na zawsze</button>
+					</form>
+						
+		<a href="filemanager.php"><button type='submit' class="btn btn-primary left cancel">Anuluj</button><a>
+	</div>
   </body>
 </html>
