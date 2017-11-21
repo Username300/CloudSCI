4043e5f2216e1d861e3ab737d06a0fa3cbd8acd0
kamilawk19 <bloghoran@gmail.com>
2017-11-21 16:46:13 +0100

    Update registerform.php

--- engine/registerform.php
+++ engine/registerform.php
@@ -18,7 +18,7 @@ include('config.php');
 	   <title>Zarejestruj się - <?php echo $project_title; ?></title>
 	   <meta charset="utf-8">
 	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	   <link rel="stylesheet" href="..\css\clouds.css">
+	   <link rel="stylesheet" href="..\css\register_form.css">
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -46,7 +46,7 @@ include('config.php');
 			  
 		</form>
 		<br>
-		<div style="text-align: center;"><a href="index.php"><button type='button' class="btn btn-danger">Wróć do strony głównej</button></a></div>
+		<div style="text-align: center;"><a href="../index.php"><button type='button' class="btn btn-danger">Wróć do strony głównej</button></a></div>
 		<br>
 	  
 	</div>
