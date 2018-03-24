c2f1b2567cf96a7c209616bc54cd4546810dd288
kamilawk19 <bloghoran@gmail.com>
2018-03-24 20:02:14 +0100

    Update panelstat.php

--- engine/panelstat.php
+++ engine/panelstat.php
@@ -1,1 +1,72 @@
-//
+<?php
+require_once("config.php");
+require_once("addons.php");
+session_start();
+$connect = new mysqli($dbhost, $dbusername, $dbpassword, $dbname);
+if(!isset($_SESSION['login'])){ //sprawdzanie czy zalogowany
+  header("Location: ../index.php");
+  die();
+}
+$login = $_SESSION['login'];
+$stat = new Statistics($connect, $login, $dbprefix); //dostep do statystyk
+if(!isset($_GET['pid'])) $current_dir=0; //sprawdzanie, w jakim katalogu sie znajdujemy
+else{
+  $current_dir=secure_string($connect, $_GET['pid']);
+}
+if(mysqli_connect_errno()==0) //pobieranie danych z bazy
+{
+  if($current_dir!=0){ //ustalanie nazwy katalogu, w ktorym jestesmy
+    $result = $connect->query("SELECT name,pid,type FROM files$dbprefix WHERE id='$current_dir' AND owner='$login'");
+    $row = $result->fetch_assoc();
+    $dir_name = $row['name'];
+    $dir_parent = $row['pid'];
+    $dir_verify = $row['type'];
+    if($dir_verify !== "DIR"){
+      header("Location: ../index.php"); //Blad: obiekt nie jest katalogiem
+      die();
+    }
+  }
+  else{
+    $dir_name = "Katalog główny";
+    $dir_parent = 0;
+  }
+  //pobieranie listy plikow
+ $files = array();
+ $result = $connect->query("SELECT id,name,ext,type,updated,size FROM files$dbprefix WHERE pid='$current_dir' AND owner='$login'");
+ while($row = $result->fetch_assoc()){
+ $files[] = $row;
+ }
+ $num_of_files = count($files);
+}
+?>
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+	  <!--
+  //////////////////////////////
+  //                          //
+  // Pamiętaj, by dodać CSS   //
+  //   i pliki bootstrapa     //
+  //                          //
+  //////////////////////////////
+  -->
+	   <title> <?php echo $dir_name." - Pliki użytkownika ".$login." - ".$project_title; ?></title>
+	   <meta charset="utf-8">
+
+	   <meta charset="utf-8">
+	   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	   <link rel="stylesheet" href="..\css\filemanager.css">
+
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Font-Awesome -->
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+  
+<div class="container"><h2>Strona w budowie</h2></div>  
+  
+  
+</body>
+</html>
